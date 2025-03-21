03cd50641f1c5e4bbae2c79bf7fc934f8e93a1e8
Gianpaolo Reinares <gprcards@gmail.com>
2025-03-21 16:06:54 -0700

    PLEASE

--- src/services/localstorage.ts
+++ src/services/localstorage.ts
@@ -1,5 +1,5 @@
 function saveToLocalStorage(country: string) {
-    try {
+
         if (typeof window !== 'undefined') {
             const countryArr = getLocalStorage();
             if (!countryArr.includes(country)) {
@@ -7,61 +7,57 @@ function saveToLocalStorage(country: string) {
             }
             localStorage.setItem('Country', JSON.stringify(countryArr));
         }
-    } catch (error) {
-        console.error('Error while setting token in localStorage:', error);
-    }
    
 
 }
 function saveToRecentStorage(country: string) {
-    try {
+
         if (typeof window !== 'undefined') {
 
             const recentArr = getLocalStorage();
             if (!recentArr.includes(country)) {
                 recentArr.push(country);
             }
-            localStorage.setItem('Recent', JSON.stringify(recentArr));        }
-    } catch (error) {
-        console.error('Error while setting token in localStorage:', error);
-    }
+            localStorage.setItem('Recent', JSON.stringify(recentArr));   
+             }
+    
 
 
 }
 
 function getRecentStorage(){
-    const localStorageData = localStorage.getItem('Recent');
+    if(typeof window != null)
+    {
+        const localStorageData = localStorage.getItem('Recent');
     if (localStorageData == null) {
         return [];
     }
     return JSON.parse(localStorageData);
+    }
+    
 }
 
 function getLocalStorage(){
 
-    try {
         if (typeof window !== 'undefined') {
             const localStorageData = localStorage.getItem('Country');
             if (localStorageData == null) {
                 return [];
             }
-            return JSON.parse(localStorageData);        }
-    } catch (error) {
-        console.error('Error while setting token in localStorage:', error);
-    }
+            return JSON.parse(localStorageData);      
+          }
+ 
     
 }
 
 function removeFromLocalStorage(country: string){
-    try {
+    
         if (typeof window !== 'undefined') {
             const countryArr = getLocalStorage();
             const countryindex = countryArr.indexOf(country);
             countryArr.splice(countryindex, 1);
-            localStorage.setItem('Country', JSON.stringify(countryArr));        }
-    } catch (error) {
-        console.error('Error while setting token in localStorage:', error);
-    }
+            localStorage.setItem('Country', JSON.stringify(countryArr));     
+           }
 
     
 }
